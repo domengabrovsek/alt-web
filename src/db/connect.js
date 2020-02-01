d1d8c5aa9b2c65f76f8675a77cc09aecd879d1c0
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2020-02-01 15:50:55 +0100

    Added config for credentials and connection setup for db

---
+++ src/db/connect.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+// read config for database
+const { database } = require('../../configuration.json');
+
+// set additional options
+const options = {
+  host: database.host,
+  dialect: database.dialect,
+  define: {
+    freezeTableName: true, // disable auto pluralizing table names
+    timestamps: false // disable creating created_at and updated_at columns
+  }
+}
+
+// create the connection
+const sequelize = new Sequelize(database.name, database.user, database.password, options);
+
+// check if connection is working
+sequelize
+  .authenticate()
+  .catch(error => sequelize = undefined);
+
+module.exports = sequelize;
