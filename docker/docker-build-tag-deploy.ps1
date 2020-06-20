c607f5cf32d0cced35854229c647f701a0cb10c3
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2020-06-20 14:14:24 +0200

    Refactored docker image to only have 1 container

--- docker/docker-build-tag-deploy.ps1
+++ docker/docker-build-tag-deploy.ps1
@@ -1,22 +1,16 @@
-# script to build and push docker images for alt-web server/client
+# script to build and push docker image for alt-web app
 
-"server", "client" | ForEach-Object -Process {
+# build the image
+Write-Host "Building docker image..." -ForegroundColor Cyan;
+Invoke-Expression "docker build -t alt-web-app .";
+Write-Host "Docker image built." -ForegroundColor Cyan;
 
-  # just to be clear what is what
-  $name = $_;
+# tag the image
+Write-Host "Tagging docker image..." -ForegroundColor Cyan;
+Invoke-Expression "docker tag alt-web-app domengabrovsek/alt-web:app"
+Write-Host "Docker image for tagged." -ForegroundColor Cyan;
 
-  # build the image
-  Write-Host "Building image for $name..." -ForegroundColor Cyan;
-  Invoke-Expression "docker build -t alt-web-$name ../docker/$name";
-  Write-Host "Image for $name built." -ForegroundColor Cyan;
-
-  # tag the image
-  Write-Host "Tagging image for $name..." -ForegroundColor Cyan;
-  Invoke-Expression "docker tag alt-web-$name domengabrovsek/alt-web:$name"
-  Write-Host "Image for $name tagged." -ForegroundColor Cyan;
-
-  # push the image to docker hub
-  Write-Host "Pushing image for $name to docker hub..." -ForegroundColor Cyan;
-  Invoke-Expression "docker push domengabrovsek/alt-web:$name"
-  Write-Host "Image for $name pushed to docker hub." -ForegroundColor Cyan;
-}
+# push the image to docker hub
+Write-Host "Pushing docker image to docker hub..." -ForegroundColor Cyan;
+Invoke-Expression "docker push domengabrovsek/alt-web:app"
+Write-Host "Docker image pushed to docker hub." -ForegroundColor Cyan;
