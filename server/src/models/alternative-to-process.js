3ae66dc410463b868554d834ae13b4bac4d4a048
Domen Gabrovsek <domen.gabrovsek@gmail.com>
2020-10-03 13:27:53 +0200

    Added alternative-to-process table

--- server/src/models/alternative-to-process.js
+++ server/src/models/alternative-to-process.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize(
-  process.env.ALT_WEB_DB_NAME,
-  process.env.ALT_WEB_DB_USER,
-  process.env.ALT_WEB_DB_PASSWORD, {
-  host: process.env.ALT_WEB_HOST,
-  dialect: process.env.ALT_WEB_DIALECT,
-  logging: false,
-  freezeTableName: true
-});
+const { sequelize, Sequelize } = require('../config/options');
 
 const Alternative = sequelize.define('ALTERNATIVE_TO_PROCESS', {
   website_id: { type: Sequelize.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
